e63a50159026cb7a3aaa04dae54cbf18fafc3799
Dennis Sun <dennis.a.sun@gmail.com>
2023-04-28 11:24:55 -0700

    updated aggregate lists to just take a list of file paths instead of doing file path handling internally

--- utils/aggregate_lists.py
+++ utils/aggregate_lists.py
@@ -6,40 +6,19 @@ from pathlib import Path
 # parse command line arguments
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", nargs = '+', help = 'Names of input directories.')
-    parser.add_argument("-s", "--suffix", nargs = '+', help = 'Suffix of each output file in each directory.')
-    parser.add_argument("-o", "--output", help = 'Name of output file.')
+    parser.add_argument("-i", "--input", nargs = '+', help = 'Paths of input files.')
+    parser.add_argument("-o", "--output", help = 'Path of output file.')
     args = parser.parse_args()
     
     return args
 
 # aggregate .txt files from folders based on suffix
-def aggregate_lists(input_dirs: list, input_suffixes: list, output_file: str):
-    
-    # make sure that input directories and suffixes are of same length
-    if len(input_dirs) != len(input_suffixes):
-        sys.exit('Number of input directories and suffixes does not match. Please try again.')
-        
-    # zip input directories and suffixes into dict
-    input_dict = dict(zip(input_dirs, input_suffixes))
-
-    # empty list to collect file paths
-    files = []
-    
-    # iterate through input dictionary
-    for directory, suffix in input_dict.items():
-        # make Path object for location
-        directory_Path = Path(directory)
-        # generate list of filepaths
-        matches = [directory_Path / i for i in os.listdir(directory) if suffix in i]
-        # append to files list
-        files = files + matches
-
+def aggregate_lists(input_files: list, output_file: str):
     # empty set to collect ids and prevent collisions
     id_set = set()
     
     # iterate through files
-    for file in files:
+    for file in input_files:
         # get file contents
         with open(file, 'r') as f:
             ids = f.read().splitlines()
@@ -53,10 +32,9 @@ def aggregate_lists(input_dirs: list, input_suffixes: list, output_file: str):
 # run this if called from the interpreter
 def main():
     args = parse_args()
-    input_dirs = args.input
-    input_suffixes = args.suffix
+    input_files = args.input
     output_file = args.output
-    aggregate_lists(input_dirs, input_suffixes, output_file)
+    aggregate_lists(input_files, output_file)
 
 # check if called from interpreter
 if __name__ == '__main__':
