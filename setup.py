79642aa4d650fa4228059acadefc24864140ac42
Dennis Sun <dennis.a.sun@gmail.com>
2023-05-15 13:19:59 -0700

    updated setup file with additional scripts

--- setup.py
+++ setup.py
@@ -11,14 +11,22 @@ setup(
     version='0.0.1',
     license='MIT',
     description='Builds maps of protein space from structures.',
-    scripts=['ProteinCartography/aggregate_lists.py',
+    scripts=['ProteinCartography/aggregate_features.py',
+             'ProteinCartography/aggregate_lists.py',
+             'ProteinCartography/dim_reduction.py
              'ProteinCartography/esmfold_apiquery.py',
              'ProteinCartography/extract_foldseekhits.py',
+             'ProteinCartography/extract_input_distances.py',
              'ProteinCartography/fetch_accession.py',
              'ProteinCartography/foldseek_apiquery.py',
              'ProteinCartography/foldseek_clustering.py',
+             'ProteinCartography/get_source.py',
+             'ProteinCartography/leiden_clustering.py',
              'ProteinCartography/make_dummies.py',
              'ProteinCartography/map_refseqids.py',
+             'ProteinCartography/plot_interactive.py',
+             'ProteinCartography/query_uniprot.py',
+             'ProteinCartography/rescue_mapping.py',
              'ProteinCartography/run_blast.py']
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
